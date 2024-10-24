6ec25c91e348cfef138bf4a21482c6567083342b
JDSweetBeat <32932271+JDSweet@users.noreply.github.com>
2024-10-24 15:43:40 -0500

    Fixed a bug where the selected star system wasn't actually being selected by the GameInstance.

--- assets/scripts/gameplay/00_star_system_screen_gameplay_callbacks.lua
+++ assets/scripts/gameplay/00_star_system_screen_gameplay_callbacks.lua
@@ -3,7 +3,7 @@ planet_overview_window_visible = false;
 
 
 function on_click(touchPos, star_system, scene, game_instance, game_state)
-    window = scene:getWidgetByID('planet_overview_window')
+    local window = scene:getWidgetByID('planet_overview_window')
     if window:isVisible() then
         window:setVisible(false)
     end
@@ -11,9 +11,18 @@ function on_click(touchPos, star_system, scene, game_instance, game_state)
 end
 
 function on_planet_clicked(touchPos, planet, scene, game_instance, game_state)
-    window = scene:getWidgetByID('planet_overview_window')
+    game_instance:selectPlanet(planet)
+
+    local window = scene:getWidgetByID('planet_overview_window')
+    local titleLabel = window:getTitleLabel()
+    titleLabel:setText('Planet ' .. planet:getID())
     if not window:isVisible() then
         window:setVisible(true)
     end
+
+    local hab_label = scene:getWidgetByID('planet_habitability_label')
+    local size_label = scene:getWidgetByID('planet_size_label')
+    hab_label:setText('Habitability: ' .. game_instance:getSelectedPlanet():getHabitabilityRounded())
+    size_label:setText('Planet Size: ' .. game_instance:getSelectedPlanet():getSize())
     print('[Lua Debug] Planet ' .. planet.id .. " selected.")
 end
